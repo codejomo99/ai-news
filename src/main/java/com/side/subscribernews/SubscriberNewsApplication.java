fa5561723eb5a626df5de6ef839f0cf0a78646e3
Jong-Min <codejomo99@gmail.com>
2025-06-12 17:05:56 +0900

    git init

---
+++ src/main/java/com/side/subscribernews/SubscriberNewsApplication.java
@@ -0,0 +1,13 @@
+package com.side.subscribernews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriberNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriberNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/side/subscribernews/SubscriberNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side.subscribernews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriberNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
